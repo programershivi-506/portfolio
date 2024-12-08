2fb127ffd87cadfa69143dc38ba1b89b368674d8
programershivi-506 <105768565+programershivi-506@users.noreply.github.com>
2024-12-08 16:43:40 +0530

    added portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn-1').addEventListener('click', function () {
+    alert('Thank you for clicking!');
+});
+
+document.querySelector('.btn-2').addEventListener('click', function () {
+    alert('Thank you for clicking');
+})
+
+
